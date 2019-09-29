b3ecfb3a4195f22f2c0f1f95a772c31266948d75
Ernesto Alarcón Gallo <ernesto.alarcon.gallo@gmail.com>
2019-09-29 23:21:13 +0200

    update cofiCostFunction

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -43,6 +43,9 @@ Theta_grad = zeros(size(Theta));
 
 J = (1/2) * sum(sum(((X*Theta'-Y).^2)(R==1)));
 
+Theta_grad = ((X*Theta'-Y).*R) * Theta;
+X_grad = ((X*Theta'-Y).*R)' * X;
+
 
 % =============================================================
 
